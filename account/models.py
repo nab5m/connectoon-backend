aad36ca9e6e086dd4e3df9f994f5034e00ab3e9f
nab5m <sk990420@naver.com>
2020-03-31 16:25:49 +0900

    Fix unused import

--- account/models.py
+++ account/models.py
@@ -2,7 +2,6 @@ import os
 import uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
